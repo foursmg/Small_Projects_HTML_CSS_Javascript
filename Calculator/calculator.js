cc3ac156f3e02a9d9272a4d3ea760e285fd14b0b
foursmg <141165954+foursmg@users.noreply.github.com>
2026-06-01 10:05:05 +0530

    ...

---
+++ Calculator/calculator.js
@@ -0,0 +1,19 @@
+/*
+let inputvalue = '';
+function inputVal(x){
+   // let x = document.querySelectorAll('.button').value;
+    if(x!= "C" && x!="="){
+        inputvalue += x;
+    }
+    document.querySelector('#display').value = inputvalue;
+    if(x== "="){
+        document.querySelector("#display").value = inputvalue+`=`+eval(inputvalue);
+        inputvalue= eval(inputvalue);
+    }
+    if(x == "C"){
+        inputvalue= "";
+       // document.querySelector("#display").value = inputvalue;
+    }
+    document.querySelector("#display").value = inputvalue; // remove this if dont want to overwrite full expression
+}
+*/
