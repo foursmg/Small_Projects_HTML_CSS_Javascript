042e99141c335bbe46a5c0637468bffd5ced6428
foursmg <141165954+foursmg@users.noreply.github.com>
2026-06-03 01:19:04 +0530

    ...

--- Rock Paper Scissors/RockPS.js
+++ Rock Paper Scissors/RockPS.js
@@ -1,4 +1,4 @@
-
+let userScore= 0; let botScore=0; 
 function game(x){
     let userVal =x;
     let y = Math.random();
@@ -11,39 +11,39 @@ function game(x){
         botVal='scissor';
     }
 
-    if(userVal== 'rock' && botVal=='rock'){
-        document.querySelector('#userdisplay').innerText= 'Rock';
-        document.querySelector('#winsdisplay').innerText= 'TIE';
-        document.querySelector('#botdisplay').innerText= 'Rock';
-        document.querySelector('winsdisplay').style.backgroundColor='white';
-    }
-    if(userVal== 'rock' && botVal=='paper'){
-        document.querySelector('#userdisplay').innerText= 'Rock';
-        document.querySelector('#winsdisplay').innerText= 'Paper wins';
-        document.querySelector('#botdisplay').innerText= 'Paper';
-        
-    }
-    if(userVal== 'rock' && botVal=='scissor'){
-        document.querySelector('#userdisplay').innerText= 'Rock';
-        document.querySelector('#winsdisplay').innerText= 'Scissor wins';
-        document.querySelector('#botdisplay').innerText= 'Scissor';
-        
-    }
-    if(userVal== 'paper' && botVal=='paper'){
-        document.querySelector('#userdisplay').innerText= 'Paper';
-        document.querySelector('#winsdisplay').innerText= 'TIE';
-        document.querySelector('#botdisplay').innerText= 'paper';
-        document.querySelector('winsdisplay').style.backgroundColor='white';
-    }
-    if(userVal== 'paper' && botVal=='scissor'){
-        document.querySelector('#userdisplay').innerText= 'Paper';
-        document.querySelector('#winsdisplay').innerText= 'scissor';
-        document.querySelector('#botdisplay').innerText= 'Scissor';
-    }
-    if(userVal== 'scissor' && botVal=='scissor'){
-        document.querySelector('#userdisplay').innerText= 'scissor';
-        document.querySelector('#winsdisplay').innerText= 'TIE'
-        document.querySelector('#botdisplay').innerText= 'Scissor';
-        document.querySelector('winsdisplay').style.backgroundColor='white';
-    }
+    
+
+    document.querySelector('#userdisplay').innerText = userVal;
+    document.querySelector('#botdisplay').innerText = botVal;
+    let winner = document.querySelector('#winsdisplay');
+
+    let playerScore= document.querySelector('#userScore');
+    let computerScore = document.querySelector('#botScore');
+
+   if(userVal == botVal){
+          winner.innerText = 'TIE';
+          document.querySelector('#winsdisplay').style.backgroundColor = 'yellow';
+   }else if((userVal=='rock' && botVal=='scissor')||(userVal=='paper' && botVal=='rock')|| (userVal=='scissor' && botVal=='paper')){
+        winner.innerText = userVal;
+        userScore++;
+        playerScore.innerText='You: '+ userScore;
+        document.querySelector('#winsdisplay').style.backgroundColor = 'teal';
+   }else{
+    winner.innerText = botVal;
+    botScore++;
+    computerScore.innerText='Bot: '+ botScore;
+    document.querySelector('#winsdisplay').style.backgroundColor = 'turquoise';
+   }
+}
+
+function reset(){
+    userScore=0;
+    botScore=0;
+    document.querySelector('#userScore').innerText='You: '+ userScore;
+    document.querySelector('#botScore').innerText='Bot: '+ botScore;
+    document.querySelector('#userdisplay').innerText = 'You';
+    document.querySelector('#botdisplay').innerText = 'Bot';
+    document.querySelector('#winsdisplay').innerText = 'wins';
+    document.querySelector('#winsdisplay').style.backgroundColor = 'rgb(143, 213, 45)';
+    
 }
